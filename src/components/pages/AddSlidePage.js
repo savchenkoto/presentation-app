5246cecd9aa81cc1277f0a8ba2e3333e90c9b3fd
savchenkoto <lapital36@gmail.com>
2018-10-30 14:32:38 +0300

    added simple form on addSlide page

--- src/components/pages/AddSlidePage.js
+++ src/components/pages/AddSlidePage.js
@@ -3,7 +3,17 @@ import React from 'react'
 const AddSlidePage = () => {
   return (
     <div>
-
+      <form>
+        <label>
+          Position
+          <input type='number' min={1}/>
+        </label>
+        <label>
+          Image
+          <input type="file" accept='image/*'/>
+        </label>
+        <input type="submit"/>
+      </form>
     </div>
   )
 }

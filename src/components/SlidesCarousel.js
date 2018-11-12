a26ae9d71f272ff99dafb62cffb2cc0bd22a22eb
savchenkoto <lapital36@gmail.com>
2018-11-12 15:54:29 +0300

    added carousel logic

--- src/components/SlidesCarousel.js
+++ src/components/SlidesCarousel.js
@@ -1,14 +1,39 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Slide from './Slide'
+import '../styles/slideCarousel.css'
+import { selectSlide } from '../actions/slides'
 
-const SlidesCarousel = ({ slides, activeSlideId }) => {
-  const activeSlide = activeSlideId ?
-    slides.find(slide => slide.id === activeSlideId) :
-    slides.length ? slides[0] : ''
+const SlidesCarousel = ({ slides, activeSlideId, selectSlide }) => {
+
+  const activeSlide = activeSlideId && slides.find(slide => slide.id === activeSlideId)
+  const currentIndex = activeSlideId && slides.findIndex(item => item.id === activeSlideId)
+
+  const nextSlide = (index) => {
+    index < slides.length - 1 && selectSlide(slides[index + 1].id)
+  }
+
+  const prevSlide = (index) => {
+    index > 0 && selectSlide(slides[index - 1].id)
+  }
+
   return (
     <div className='box carousel-grid'>
-      <Slide slide={activeSlide}/>
+      <div
+        className='navigate left'
+        onClick={() => prevSlide(currentIndex)}
+      >
+        <span>{`<`}</span>
+      </div>
+      <div className='content'>
+        <Slide slide={activeSlide}/>
+      </div>
+      <div
+        className='navigate right'
+        onClick={() => nextSlide(currentIndex)}
+      >
+        <span>{`>`}</span>
+      </div>
     </div>
   )
 }
@@ -18,4 +43,8 @@ const mapStateToProps = (state) => ({
   activeSlideId: state.activeSlideId
 })
 
-export default connect(mapStateToProps)(SlidesCarousel)
+const mapDispatchToProps = (dispatch) => ({
+  selectSlide: (slideId) => dispatch(selectSlide(slideId))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SlidesCarousel)
